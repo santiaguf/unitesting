14f02c31c24dfb9873ab6c8876d1682d4b9de05c
Santiago Bernal <exactlimon@gmail.com>
2022-09-02 17:15:12 -0500

    feat: StringUtil

---
+++ src/main/java/org/example/unitesting/util/StringUtil.java
@@ -0,0 +1,13 @@
+package org.example.unitesting.util;
+
+public class StringUtil {
+    public static String repeat(String str, int times ){
+        String result = "";
+
+        for (int i = 0; i < times; i++) {
+            result += str;
+        }
+
+        return result;
+    }
+}
