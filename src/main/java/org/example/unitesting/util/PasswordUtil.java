865e5dd283148ae6594af0bd7298556fbff06634
Santiago Bernal <exactlimon@gmail.com>
2022-09-02 17:16:51 -0500

    feat: passwordUtil

---
+++ src/main/java/org/example/unitesting/util/PasswordUtil.java
@@ -0,0 +1,13 @@
+package org.example.unitesting.util;
+
+public class PasswordUtil {
+
+
+    public enum SecurityLevel {
+        WEAK, MEDIUM, STRONG
+    }
+
+    public SecurityLevel assessPassword(String password){
+        return null;
+    }
+}
