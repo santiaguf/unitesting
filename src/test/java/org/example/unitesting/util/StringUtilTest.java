3f715d0c1ff42f6ae916ba8eb175c39580ce9e92
Santiago Bernal <exactlimon@gmail.com>
2022-09-05 18:05:55 -0500

    [update] test with trow runtimeException

--- src/test/java/org/example/unitesting/util/StringUtilTest.java
+++ src/test/java/org/example/unitesting/util/StringUtilTest.java
@@ -3,16 +3,17 @@ package org.example.unitesting.util;
 class StringUtilTest {
     public static void main(String[] args) {
         String result = StringUtil.repeat("hola", 3);
-        System.out.println(result);
-
-        if(!result.equals("holaholahola")){
-            System.out.println("error");
-        }
+        assertEquals(result, "holaholahola");
 
         String result2 = StringUtil.repeat("hola", 1);
-        System.out.println(result);
         if(!result2.equals("hola")){
-            System.out.println("error");
+            throw new RuntimeException("error ");
+        }
+    }
+
+    private static void assertEquals(String actual, String expected) {
+        if(!actual.equals(expected)){
+            throw new RuntimeException(actual + " is not equal to " + expected);
         }
     }
 }
