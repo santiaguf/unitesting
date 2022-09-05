973e790d7a6e568026b57df40eaae91f34a3816d
Santiago Bernal <exactlimon@gmail.com>
2022-09-05 18:54:28 -0500

    [test] created new test for StringUtil

--- src/main/java/org/example/unitesting/util/StringUtil.java
+++ src/main/java/org/example/unitesting/util/StringUtil.java
@@ -2,6 +2,11 @@ package org.example.unitesting.util;
 
 public class StringUtil {
     public static String repeat(String str, int times ){
+
+        if(times < 0){
+            throw new IllegalArgumentException("negative times not allowed");
+        }
+
         String result = "";
 
         for (int i = 0; i < times; i++) {
