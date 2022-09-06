43e751caa38d1c5dfb1ce277cd9c188532401a75
Santiago Bernal <exactlimon@gmail.com>
2022-09-05 20:02:19 -0500

    test: add dice tests with mockito

--- src/test/java/org/example/unitesting/player/DiceTest.java
+++ src/test/java/org/example/unitesting/player/DiceTest.java
@@ -1,6 +1,7 @@
 package org.example.unitesting.player;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
@@ -8,6 +9,13 @@ public class DiceTest {
 
     @Test
     public void return_a_number_between_1_and_6() {
+        Dice dice = Mockito.mock(Dice.class);
+
+        assertTrue("is lower than six", dice.roll() < 6);
+    }
+
+    @Test
+    public void return_two() {
         Dice dice = new Dice(6);
 
         assertTrue("is lower than six", dice.roll() < 6);
