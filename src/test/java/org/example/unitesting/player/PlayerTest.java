b67b2b322fc400885671f7f291973213bc99e542
Santiago Bernal <exactlimon@gmail.com>
2022-09-05 19:37:15 -0500

    test: add test when dice number is big

--- src/test/java/org/example/unitesting/player/PlayerTest.java
+++ src/test/java/org/example/unitesting/player/PlayerTest.java
@@ -16,4 +16,14 @@ public class PlayerTest {
         Player player = new Player(dice, 3);
         assertFalse(player.play());
     }
+
+    @Test
+    public void wins_when_dice_number_is_big() {
+        Dice dice = Mockito.mock(Dice.class);
+
+        Mockito.when(dice.roll()).thenReturn(4);
+
+        Player player = new Player(dice, 3);
+        assertTrue(player.play());
+    }
 }
