b2fa26f087bb31b538f91ac22cd8d46e1e8e4132
Santiago Bernal <exactlimon@gmail.com>
2022-09-06 17:02:31 -0500

    feat: DateUtil is leap year

---
+++ src/main/java/org/example/unitesting/util/DateUtil.java
@@ -0,0 +1,8 @@
+package org.example.unitesting.util;
+
+public class DateUtil {
+
+    public static boolean isLeapYear(int year){
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
