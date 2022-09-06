1f80dc878b2efeda6e080fd6ae0592256fc8223b
Santiago Bernal <exactlimon@gmail.com>
2022-09-06 18:22:37 -0500

    test: created movie service test

---
+++ src/test/java/org/example/unitesting/movies/service/MovieServiceShould.java
@@ -0,0 +1,44 @@
+package org.example.unitesting.movies.service;
+
+import org.example.unitesting.movies.data.MovieRepository;
+import org.example.unitesting.movies.model.Genre;
+import org.example.unitesting.movies.model.Movie;
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
+
+public class MovieServiceShould {
+
+    @Test
+    public void return_movies_by_genre() {
+
+        MovieRepository movieRepository = Mockito.mock(MovieRepository.class);
+
+        Mockito.when(movieRepository.findAll()).thenReturn(
+                Arrays.asList(
+                        new Movie(1, "Dark Knight", 152, Genre.ACTION),
+                        new Movie(2, "Memento", 113, Genre.THRILLER),
+                        new Movie(3, "There's Something About Mary", 119, Genre.COMEDY),
+                        new Movie(4, "Super 8", 112, Genre.THRILLER),
+                        new Movie(5, "Scream", 111, Genre.HORROR),
+                        new Movie(6, "Home Alone", 103, Genre.COMEDY),
+                        new Movie(7, "Matrix", 136, Genre.ACTION)
+                )
+        );
+
+        MovieService movieService = new MovieService(movieRepository);
+
+        Collection<Movie> movies = movieService.findMoviesByGenre(Genre.COMEDY);
+
+        List<Integer> movieIds = movies.stream().map(movie -> movie.getId()).collect(Collectors.toList());
+
+        assertThat(movieIds, CoreMatchers.is(Arrays.asList(3, 6)));
+    }
+}
