de7c38d90b84fb941b804328ec43c1760faa2f16
Santiago Bernal <exactlimon@gmail.com>
2022-09-05 19:33:20 -0500

    Test: first mock with mockito

--- src/test/java/org/example/unitesting/player/PlayerTest.java
+++ src/test/java/org/example/unitesting/player/PlayerTest.java
@@ -1,6 +1,7 @@
 package org.example.unitesting.player;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
@@ -8,8 +9,11 @@ public class PlayerTest {
 
     @Test
     public void loses_when_dice_number_is_to_low() {
-        Dice dice = new Dice(6);
-        Player player = new Player(dice, 2);
+        Dice dice = Mockito.mock(Dice.class);
+
+        Mockito.when(dice.roll()).thenReturn(2);
+
+        Player player = new Player(dice, 3);
         assertEquals(false, player.play());
     }
 }
