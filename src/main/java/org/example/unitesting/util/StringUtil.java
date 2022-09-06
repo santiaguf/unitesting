cfd7ae498b1c3b72df18e6568c72b6baf6437cdc
Santiago Bernal <exactlimon@gmail.com>
2022-09-06 16:26:21 -0500

    feat: isEmpty function with test

--- src/main/java/org/example/unitesting/util/StringUtil.java
+++ src/main/java/org/example/unitesting/util/StringUtil.java
@@ -15,4 +15,8 @@ public class StringUtil {
 
         return result;
     }
+
+    public static boolean isEmpty(String str){
+        return str == null || str.trim().length() < 1;
+    }
 }
--- src/test/java/org/example/unitesting/util/StringUtilTest.java
+++ src/test/java/org/example/unitesting/util/StringUtilTest.java
@@ -27,6 +27,27 @@ public class StringUtilTest {
         Assert.assertEquals("", StringUtil.repeat("hola", -1));
     }
 
+    @Test
+    public void word_is_not_empty(){
+        Assert.assertFalse(StringUtil.isEmpty("Palabra"));
+    }
+
+    @Test
+    public void quotationMarks_is_Empty(){
+        Assert.assertTrue(StringUtil.isEmpty(""));
+    }
+
+    @Test
+    public void space_is_Empty(){
+        String str = new String(" ");
+        Assert.assertTrue(StringUtil.isEmpty(str));
+    }
+
+    @Test
+    public void null_is_empty(){
+        Assert.assertTrue(StringUtil.isEmpty(null));
+    }
+
     /* available in Junit 5+
     @Test
     public void repeat_string_negative_times_assert_throw() {
