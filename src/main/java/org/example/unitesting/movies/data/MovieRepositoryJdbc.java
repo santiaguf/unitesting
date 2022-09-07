fe347a73822640f381dc240da1d0a269019a26b8
Santiago Bernal <exactlimon@gmail.com>
2022-09-07 10:50:45 -0500

    test: movie repository integration

--- src/main/java/org/example/unitesting/movies/data/MovieRepositoryJdbc.java
+++ src/main/java/org/example/unitesting/movies/data/MovieRepositoryJdbc.java
@@ -1,10 +1,27 @@
 package org.example.unitesting.movies.data;
 
+import org.example.unitesting.movies.model.Genre;
 import org.example.unitesting.movies.model.Movie;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import java.util.Collection;
 
 public class MovieRepositoryJdbc implements MovieRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    private static RowMapper<Movie> movieMapper = (rs, rowNum) ->
+            new Movie(
+                    rs.getInt("id"),
+                    rs.getString("name"),
+                    rs.getInt("minutes"),
+                    Genre.valueOf(rs.getString("genre")));
+
+    public MovieRepositoryJdbc(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public Movie findById(long id) {
         return null;
@@ -12,8 +29,10 @@ public class MovieRepositoryJdbc implements MovieRepository {
 
     @Override
     public Collection<Movie> findAll() {
-        return null;
+        return jdbcTemplate.query("select * from movies", movieMapper);
+
     }
+
 
     @Override
     public void saveOrUpdate(Movie movie) {
